242834d70fc5c6e21fe512e5641e87cb29fa963a
Daniel Jovino <76604785+DanielJovino@users.noreply.github.com>
2021-12-21 13:41:01 -0300

    Create aula8.py

---
+++ aula8/aula8.py
@@ -0,0 +1,18 @@
+"""
+Entrada de dados
+"""
+"""
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+ano_nascimento = 2021 - int(idade)
+
+print()
+print(f'{nome} tem {idade} anos.' 
+      f'{nome} nasceu em {ano_nascimento}')
+      """
+
+numero_1 = int(input('Digite um némero: ') )
+numero_2 = input('Digite um némero: ')
+numero_2 = int(numero_2)
+
+print( numero_1 + numero_2 )
