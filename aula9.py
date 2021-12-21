5dbce5e0cc69504186a8a3caba7c395e0814271a
Daniel Jovino <76604785+DanielJovino@users.noreply.github.com>
2021-12-21 13:41:48 -0300

    Create aula9.py

---
+++ aula9.py
@@ -0,0 +1,20 @@
+""""
+Condições IF, ELIF e ELSE -
+"""
+
+if False:
+    print('Verdadeiro')
+    print('teste teste2')
+elif True:
+    print("Agora é verdadeiro.")
+    nome = input("Qual é o seu nome? ")
+
+    print(f"Seu nome é {nome}.")
+elif False:
+    print('Mais uma verdadeira.')
+    print(22+22)
+else:
+    print('Não é verdadeiro.')
+    print("olá")
+
+
