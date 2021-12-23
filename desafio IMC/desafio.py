4a6fe54e5dde62687c2e155ccb261c83611d99b2
Daniel Jovino <76604785+DanielJovino@users.noreply.github.com>
2021-12-22 21:43:38 -0300

    Create desafio.py

---
+++ desafio IMC/desafio.py
@@ -0,0 +1,11 @@
+
+nome = 'Luiz'
+idade = 32
+altura = 1.8
+peso = 80
+nascimento = 2021 - idade
+imc = peso / altura **2
+
+print(f'{nome} tem {idade},{altura} de altura e pesa {peso}kg.')
+print(f'O IMC de {nome} é {imc:.2f}.')
+print(nome,'nasceu em ', nascimento)
