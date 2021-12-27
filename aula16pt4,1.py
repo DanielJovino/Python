902a7949d4129e974831055a0fed74b5e6e89cdc
Daniel Jovino <76604785+DanielJovino@users.noreply.github.com>
2021-12-27 11:31:42 -0300

    Create aula16pt4,1.py

---
+++ aula16pt4,1.py
@@ -0,0 +1,15 @@
+""""
+def - parte 4 return 
+"""
+
+variavel = 'valoir'
+
+def func():
+    outra_variavel = 'valor'
+    return outra_variavel
+
+def fun2(arg):
+    print(arg)
+
+var = func()
+fun2(var)
